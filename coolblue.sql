5b8f29f77ca6e5068727602c6a02a052156b1751
Dino Yang <s1136300@student.hsleiden.nl>
2022-10-19 23:38:33 +0200

    Update coolblue.sql

--- coolblue.sql
+++ coolblue.sql
@@ -1,15 +1,16 @@
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-
-CREATE SCHEMA IF NOT EXISTS `Coolblue` DEFAULT CHARACTER SET utf8 ;
-USE `Coolblue` ;
+-- Schema s1136300
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `s1136300` DEFAULT CHARACTER SET utf8 ;
+USE `s1136300` ;
 
 -- -----------------------------------------------------
--- Table `Coolblue`.`Klant`
+-- Table `s1136300`.`Klant`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Coolblue`.`Klant` (
+CREATE TABLE IF NOT EXISTS `s1136300`.`Klant` (
   `klant_ID` INT NOT NULL AUTO_INCREMENT,
   `naam` VARCHAR(255) NOT NULL,
   `adres` VARCHAR(255) NOT NULL,
@@ -19,32 +20,30 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Coolblue`.`Categorie`
+-- Table `s1136300`.`Categorie`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Coolblue`.`Categorie` (
+CREATE TABLE IF NOT EXISTS `s1136300`.`Categorie` (
   `categorie_ID` INT NOT NULL AUTO_INCREMENT,
   `naam` VARCHAR(255) NOT NULL,
   `omschrijving` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`categorie_ID`)
-)
+  PRIMARY KEY (`categorie_ID`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Coolblue`.`Magazijn`
+-- Table `s1136300`.`Magazijn`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Coolblue`.`Magazijn` (
+CREATE TABLE IF NOT EXISTS `s1136300`.`Magazijn` (
   `magazijn_ID` INT NOT NULL AUTO_INCREMENT,
   `adres` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`magazijn_ID`)
-)
+  PRIMARY KEY (`magazijn_ID`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Coolblue`.`Product`
+-- Table `s1136300`.`Product`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Coolblue`.`Product` (
+CREATE TABLE IF NOT EXISTS `s1136300`.`Product` (
   `prod_ID` INT NOT NULL AUTO_INCREMENT,
   `categorie_ID` INT NOT NULL,
   `magazijn_ID` INT NOT NULL,
@@ -58,21 +57,21 @@ CREATE TABLE IF NOT EXISTS `Coolblue`.`Product` (
   INDEX `fk_Producten_Magazijn1_idx` (`magazijn_ID` ASC) VISIBLE,
   CONSTRAINT `fk_Product_Category1`
     FOREIGN KEY (`categorie_ID`)
-    REFERENCES `Coolblue`.`Categorie` (`categorie_ID`)
+    REFERENCES `s1136300`.`Categorie` (`categorie_ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Producten_Magazijn1`
     FOREIGN KEY (`magazijn_ID`)
-    REFERENCES `Coolblue`.`Magazijn` (`magazijn_ID`)
+    REFERENCES `s1136300`.`Magazijn` (`magazijn_ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-    ENGINE = InnoDB;
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Coolblue`.`Orders`
+-- Table `s1136300`.`Orders`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Coolblue`.`Orders` (
+CREATE TABLE IF NOT EXISTS `s1136300`.`Orders` (
   `order_ID` INT NOT NULL AUTO_INCREMENT,
   `prod_ID` INT NOT NULL,
   `klant_ID` INT NOT NULL,
@@ -84,21 +83,21 @@ CREATE TABLE IF NOT EXISTS `Coolblue`.`Orders` (
   INDEX `fk_Orders_Klanten1_idx` (`klant_ID` ASC) VISIBLE,
   CONSTRAINT `fk_Product_copy1_Product1`
     FOREIGN KEY (`prod_ID`)
-    REFERENCES `Coolblue`.`Product` (`prod_ID`)
+    REFERENCES `s1136300`.`Product` (`prod_ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Orders_Klanten1`
     FOREIGN KEY (`klant_ID`)
-    REFERENCES `Coolblue`.`Klant` (`klant_ID`)
+    REFERENCES `s1136300`.`Klant` (`klant_ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Coolblue`.`Bestelling`
+-- Table `s1136300`.`Bestelling`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Coolblue`.`Bestelling` (
+CREATE TABLE IF NOT EXISTS `s1136300`.`Bestelling` (
   `bestelling_ID` INT NOT NULL AUTO_INCREMENT,
   `klant_ID` INT NOT NULL,
   `bezorg_adres` VARCHAR(255) NOT NULL,
@@ -107,16 +106,16 @@ CREATE TABLE IF NOT EXISTS `Coolblue`.`Bestelling` (
   INDEX `fk_Bestelling_Klanten1_idx` (`klant_ID` ASC) VISIBLE,
   CONSTRAINT `fk_Bestelling_Klanten1`
     FOREIGN KEY (`klant_ID`)
-    REFERENCES `Coolblue`.`Klant` (`klant_ID`)
+    REFERENCES `s1136300`.`Klant` (`klant_ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Coolblue`.`Bezorger`
+-- Table `s1136300`.`Bezorger`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Coolblue`.`Bezorger` (
+CREATE TABLE IF NOT EXISTS `s1136300`.`Bezorger` (
   `bezorger_ID` INT NOT NULL AUTO_INCREMENT,
   `magazijn_ID` INT NOT NULL,
   `naam` VARCHAR(255) NOT NULL,
@@ -126,16 +125,16 @@ CREATE TABLE IF NOT EXISTS `Coolblue`.`Bezorger` (
   INDEX `fk_Bezorgers_Magazijnen1_idx` (`magazijn_ID` ASC) VISIBLE,
   CONSTRAINT `fk_Bezorgers_Magazijnen1`
     FOREIGN KEY (`magazijn_ID`)
-    REFERENCES `Coolblue`.`Magazijn` (`magazijn_ID`)
+    REFERENCES `s1136300`.`Magazijn` (`magazijn_ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Coolblue`.`Inpakker`
+-- Table `s1136300`.`Inpakker`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Coolblue`.`Inpakker` (
+CREATE TABLE IF NOT EXISTS `s1136300`.`Inpakker` (
   `inpakker_ID` INT NOT NULL AUTO_INCREMENT,
   `magazijn_ID` INT NOT NULL,
   `naam` VARCHAR(255) NOT NULL,
@@ -145,16 +144,16 @@ CREATE TABLE IF NOT EXISTS `Coolblue`.`Inpakker` (
   INDEX `fk_Bezorgers_Magazijnen1_idx` (`magazijn_ID` ASC) VISIBLE,
   CONSTRAINT `fk_Bezorgers_Magazijnen10`
     FOREIGN KEY (`magazijn_ID`)
-    REFERENCES `Coolblue`.`Magazijn` (`magazijn_ID`)
+    REFERENCES `s1136300`.`Magazijn` (`magazijn_ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Coolblue`.`Bezorger_bestelling_geschiedenis`
+-- Table `s1136300`.`Bezorger_bestelling_geschiedenis`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Coolblue`.`Bezorger_bestelling_geschiedenis` (
+CREATE TABLE IF NOT EXISTS `s1136300`.`Bezorger_bestelling_geschiedenis` (
   `bezorger_ID` INT NOT NULL,
   `bestelling_ID` INT NOT NULL,
   `bezorg_datum` DATE NOT NULL,
@@ -162,21 +161,21 @@ CREATE TABLE IF NOT EXISTS `Coolblue`.`Bezorger_bestelling_geschiedenis` (
   INDEX `fk_Bezorger_bestelling_geschiedenis_Bestellingen1_idx` (`bestelling_ID` ASC) VISIBLE,
   CONSTRAINT `fk_Bezorger_bestelling_geschiedenis_Bezorgers1`
     FOREIGN KEY (`bezorger_ID`)
-    REFERENCES `Coolblue`.`Bezorger` (`bezorger_ID`)
+    REFERENCES `s1136300`.`Bezorger` (`bezorger_ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Bezorger_bestelling_geschiedenis_Bestellingen1`
     FOREIGN KEY (`bestelling_ID`)
-    REFERENCES `Coolblue`.`Bestelling` (`bestelling_ID`)
+    REFERENCES `s1136300`.`Bestelling` (`bestelling_ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Coolblue`.`Inpakker_bestelling_geschiedenis`
+-- Table `s1136300`.`Inpakker_bestelling_geschiedenis`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Coolblue`.`Inpakker_bestelling_geschiedenis` (
+CREATE TABLE IF NOT EXISTS `s1136300`.`Inpakker_bestelling_geschiedenis` (
   `inpakker_ID` INT NOT NULL,
   `bestelling_ID` INT NOT NULL,
   `bezorg_datum` DATE NOT NULL,
@@ -184,21 +183,21 @@ CREATE TABLE IF NOT EXISTS `Coolblue`.`Inpakker_bestelling_geschiedenis` (
   INDEX `fk_Inpakker_bestelling_geschiedenis_Bestelling1_idx` (`bestelling_ID` ASC) VISIBLE,
   CONSTRAINT `fk_Inpakker_bestelling_geschiedenis_Inpakker1`
     FOREIGN KEY (`inpakker_ID`)
-    REFERENCES `Coolblue`.`Inpakker` (`inpakker_ID`)
+    REFERENCES `s1136300`.`Inpakker` (`inpakker_ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Inpakker_bestelling_geschiedenis_Bestelling1`
     FOREIGN KEY (`bestelling_ID`)
-    REFERENCES `Coolblue`.`Bestelling` (`bestelling_ID`)
+    REFERENCES `s1136300`.`Bestelling` (`bestelling_ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Coolblue`.`Betaling`
+-- Table `s1136300`.`Betaling`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Coolblue`.`Betaling` (
+CREATE TABLE IF NOT EXISTS `s1136300`.`Betaling` (
   `betaling_ID` INT NOT NULL AUTO_INCREMENT,
   `klant_ID` INT NOT NULL,
   `bestelling_ID` INT NOT NULL,
@@ -209,42 +208,42 @@ CREATE TABLE IF NOT EXISTS `Coolblue`.`Betaling` (
   INDEX `fk_Betalingen_Bestellingen1_idx` (`bestelling_ID` ASC) VISIBLE,
   CONSTRAINT `fk_Betalingen_Klanten1`
     FOREIGN KEY (`klant_ID`)
-    REFERENCES `Coolblue`.`Klant` (`klant_ID`)
+    REFERENCES `s1136300`.`Klant` (`klant_ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Betalingen_Bestellingen1`
     FOREIGN KEY (`bestelling_ID`)
-    REFERENCES `Coolblue`.`Bestelling` (`bestelling_ID`)
+    REFERENCES `s1136300`.`Bestelling` (`bestelling_ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Coolblue`.`Orders_bij_bestelling`
+-- Table `s1136300`.`Orders_bij_bestelling`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Coolblue`.`Orders_bij_bestelling` (
+CREATE TABLE IF NOT EXISTS `s1136300`.`Orders_bij_bestelling` (
   `order_ID` INT NOT NULL,
   `bestelling_ID` INT NOT NULL,
   PRIMARY KEY (`bestelling_ID`, `order_ID`),
   INDEX `fk_Orders_bij_bestelling_Bestelling1_idx` (`bestelling_ID` ASC) VISIBLE,
   CONSTRAINT `fk_Orders_bij_bestelling_Order1`
     FOREIGN KEY (`order_ID`)
-    REFERENCES `Coolblue`.`Orders` (`order_ID`)
+    REFERENCES `s1136300`.`Orders` (`order_ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Orders_bij_bestelling_Bestelling1`
     FOREIGN KEY (`bestelling_ID`)
-    REFERENCES `Coolblue`.`Bestelling` (`bestelling_ID`)
+    REFERENCES `s1136300`.`Bestelling` (`bestelling_ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Coolblue`.`Klant_bestelling_geschiedenis`
+-- Table `s1136300`.`Klant_bestelling_geschiedenis`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Coolblue`.`Klant_bestelling_geschiedenis` (
+CREATE TABLE IF NOT EXISTS `s1136300`.`Klant_bestelling_geschiedenis` (
   `bestelling_ID` INT NOT NULL,
   `klant_ID` INT NOT NULL,
   `bezorg_datum` DATE NOT NULL,
@@ -253,12 +252,12 @@ CREATE TABLE IF NOT EXISTS `Coolblue`.`Klant_bestelling_geschiedenis` (
   INDEX `fk_Klant_bestelling_geschiedenis_Bestelling1_idx` (`bestelling_ID` ASC) VISIBLE,
   CONSTRAINT `fk_Klant_bestelling_geschiedenis_Klant1`
     FOREIGN KEY (`klant_ID`)
-    REFERENCES `Coolblue`.`Klant` (`klant_ID`)
+    REFERENCES `s1136300`.`Klant` (`klant_ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Klant_bestelling_geschiedenis_Bestelling1`
     FOREIGN KEY (`bestelling_ID`)
-    REFERENCES `Coolblue`.`Bestelling` (`bestelling_ID`)
+    REFERENCES `s1136300`.`Bestelling` (`bestelling_ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
